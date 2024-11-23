66c76e48fffe2ffe9b5be064f6dd7fb31f2f766b
duutabib <k.aduut@gmail.com>
2024-11-23 23:01:14 +0000

    modify: add logging (and for errors), and some comments

--- toPDF.sh
+++ toPDF.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 # Assign second argument as search dir
-EXTENSION=${1:-"epub"}
-SEARCH_DIR=${2:-"."}
-OUTPUT_DIR=${3:-"/tmp"}
+EXTENSION=${1:-"epub"} 			# default to 'epub' if no extension exist
+SEARCH_DIR=${2:-"."}			# default to curr dir 
+OUTPUT_DIR=${3:-"/tmp"}			# default to tmp dir
+LOG_FILE="${OUTPUT_DIR}/conversion.log"
+ERROR_LOG="${OUTPUT_DIR}/error.log"
 
 # check directory exits
 if [ ! -d "$SEARCH_DIR" ]; then
 	echo "Error: $SEARCH_DIR is not valid."
 	exit 1
 fi
+
+# Initialize log files
+echo "Conversion started at $(date)" > "$LOG_FILE"
+echo "Conversion errors at $(date)" > "$ERROR_LOG"
 
 # Get file of type
 echo "Searching for $EXTENSION files in $SEARCH_DIR"
