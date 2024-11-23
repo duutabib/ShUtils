a7af8c2a2ec83d59bece17bd9b39d5cd5dff86f3
duutabib <k.aduut@gmail.com>
2024-11-23 21:58:40 +0000

    modify: aviod intermediate storage

--- toPDF.sh
+++ toPDF.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Assign second argument as destination 
-SEARCH_DIR=${2:-"."}
 EXTENSION=${1:-"epub"}
+SEARCH_DIR=${2:-"."}
+OUTPUT_DIR=${3:-"/tmp"}
 
 # check directory exits
 if [ ! -d "$SEARCH_DIR" ]; then
@@ -13,14 +14,8 @@ fi
 # Get file of type
 echo "Searching for $EXTENSION files in $SEARCH_DIR"
 
-CMD='fd -e "$EXTENSION" -t f -p "$SEARCH_DIR"'
-echo "running code $CMD..."
+# search for files and convert to PDF
+fd -e "$EXTENSION" -t f -p "$SEARCH_DIR" -x ebook-convert {} "$OUTPUT_DIR/{.}.pdf"
 
-search_results=$CMD
-
-
-# get each entry in results 
-for entry in search_results
-do
-	echo "$entry"
-done
+# status statement after completion
+echo "Conversion complete. PDFs saved in: $OUTPUT_DIR"
